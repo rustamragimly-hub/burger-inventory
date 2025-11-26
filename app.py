0a951bec4214839a5f6d55def91a7db036fc4ab2
rustamragimly-hub <rustamragimly-hub@gmail.com>
2025-11-26 18:58:28 +0300

    Force redeploy

--- app.py
+++ app.py
@@ -961,3 +961,4 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 7000))
     debug = os.environ.get('FLASK_ENV') != 'production'
     app.run(host="0.0.0.0", port=port, debug=debug)
+# пустой коммит для redeploy
